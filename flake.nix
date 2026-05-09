bd89a4f4f02240d266b146ac982c1b4e80b7dd47
Devyn Freet <devyn@devynfreet.com>
2026-05-08 20:01:47 -0400

    config: added hardware configuration link to flake

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
           disko.nixosModules.disko
           { disko.devices.disk.disk1.device = "/dev/vda"; }
           ./configuration.nix
+          ./hardware-configuration.nix
         ];
       };
 
