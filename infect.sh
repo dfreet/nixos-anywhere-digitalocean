4c264f9b88fd6807792a4f301b6dc4285eedfd90
Devyn Freet <devyn@devynfreet.com>
2026-05-08 21:08:48 -0400

    chore: added infect.sh to update droplet to nixos

---
+++ infect.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "nix run github:nix-community/nixos-anywhere -- --generate-hardware-config nixos-generate-config ./hardware-configuration.nix --flake .#digitalocean --target-host root@$1"
